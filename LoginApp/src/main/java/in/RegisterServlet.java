d51a2639b21069fc07a4b068577903d74e14170f
Divu <divyeshkuchhadia@gmail.com>
2023-07-12 21:00:57 +0530

    Registration page error free

---
+++ LoginApp/src/main/java/in/RegisterServlet.java
@@ -0,0 +1,64 @@
+package in;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private static final String URL = "jdbc:mysql://localhost:3306/user";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "";
+
+	Connection connection = null;
+	Statement statement = null;
+
+	public RegisterServlet() {
+		super();
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			statement = connection.createStatement();
+			System.out.println("Connect established succesfully");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String un = request.getParameter("username");
+		String pass = request.getParameter("password");
+
+		// response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		// out.print("Registration Succesfull");
+		String query = "INSERT INTO user_tb VALUES ('" + un + "','" + pass + "');";
+		out.print(query);
+		try {
+			statement.execute(query);
+			out.println("Query Executed...");
+		} catch (SQLException e) {
+			out.println(e);
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ LoginApp/src/main/webapp/Login.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>Login</h1>
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="text" name="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <button type="submit" class="btn btn-primary" value="login">Submit</button>
+</form>
+
+</body>
+</html>
