60080290e2cfb9149d843411a6b527b804d4250c
Divu <divyeshkuchhadia@gmail.com>
2023-07-19 21:03:24 +0530

    registration servlet and login servlet is completed

--- WebApp1/src/main/java/in/adit/controller/Registration.java
+++ WebApp1/src/main/java/in/adit/controller/Registration.java
@@ -1,9 +1,11 @@
 package in.adit.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -27,12 +29,10 @@ public class Registration extends HttpServlet {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-			statement = connection.createStatement();
 			System.out.println("Connect established succesfully");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -42,14 +42,27 @@ public class Registration extends HttpServlet {
 		// TODO Auto-generated method stub
 		String un = request.getParameter("username");
 		String pass = request.getParameter("password");
+		String firstName = request.getParameter("password");
+		String lastName = request.getParameter("password");
+		String email = request.getParameter("password");
+		int mobile = Integer.parseInt(request.getParameter("number"));
 
-		// response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		// out.print("Registration Succesfull");
-		String query = "INSERT INTO user_tb VALUES ('" + un + "','" + pass + "');";
+		//response.setContentType("text/html");
+		//PrintWriter out = response.getWriter();		
 		try {
+			String query = "INSERT INTO user_info_tbl VALUES (?,?,?,?,?);";
+			statement.setString(1, un);
+			statement.setString(2, firstName);
+			statement.setString(3, lastName);
+			statement.setString(4, email);
+			statement.setInt(5, mobile);
 			statement.execute(query);
-			out.println("Query Executed...");
+			System.out.println("Query 1 Executed...");
+			String query2 = "INSERT INTO login_tbl VALUES (?,?);";
+			statement.setString(1,un);
+			statement.setString(2,pass);
+			statement.execute(query2);
+			System.out.println("Query 2 Executed...");
 		} catch (SQLException e) {
 			out.println(e);
 			e.printStackTrace();
